bfc954e4c46eefd291a097e3a332700b9dc39468
Sourabh-Ambhore <140062371+Sourabh-Ambhore@users.noreply.github.com>
2023-08-05 18:47:36 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -103,12 +103,6 @@ resource "aws_security_group" "allow_tls" {
     cidr_blocks      = ["0.0.0.0/0"]
   }
 
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
 
   ingress {
     description      = "TLS from VPC"
